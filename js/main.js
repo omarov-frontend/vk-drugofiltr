96f7461f694c2f61b23e72c3bac3612b5ed3531f
Shamil Omarov <s_omarov05@mail.ru>
2021-01-30 03:32:54 +0300

    Подключили скрипт api-Vk, сделали инициализацию приложения, так же собрали список друзей через Promise

--- js/main.js
+++ js/main.js
@@ -1,3 +1,56 @@
-const API_VK_ID = 7743958;
-const API_VK_URL = `https://api.vk.com/method/users.get?user_id=${ API_VK_ID }v=5.52`;
+const API_VK_ID = 7739054;
 
+function vkLogin() {
+    return new Promise((resolve, reject) => {
+        // инициализируем приложение
+        VK.init({
+            apiId: API_VK_ID
+        });
+
+        // авторизовываемся
+        VK.Auth.login(data => {
+            // если не удалось соединиться
+            if (data.status !== "connected") {
+                reject();
+            } else {
+                // если соединение удалось
+                resolve();
+            }
+        }, 2) // метка 2 - получение информации о друзьях
+    })
+}
+
+function renderFriends(friends, listElement) {
+    let result = '<ul class="friends-list">';
+
+    for(let i = 0; i < friends.length; i++) {
+        result += `
+        <li class="friends-list__item">
+            <img class="friends-list__img" src="${ friends[i].photo_200 }" alt="Image">
+            <div class="friends-list__name">${ friends[i].first_name + ' ' + friends[i].last_name }</div>
+            <button class="friends-list__btn friends-list__btn_remove"></button>
+        </li>
+        `
+    }
+
+    result += '</ul>';
+
+    listElement.innerHTML = result;
+}
+
+vkLogin().then(function() {
+    // успех
+    VK.api(
+        'friends.get',
+        {
+            v: '5.62',
+            fields: ['city', 'photo_200']
+        },
+        function(data) {
+            const sourceList = document.querySelector('#source-list');
+            renderFriends(data.response.items, sourceList);
+        }
+    );
+}).catch(function() {
+    // ошибка
+})
